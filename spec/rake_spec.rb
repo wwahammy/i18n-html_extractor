ec6200a616f31c9758c1da720170e81892eaa18a
German Velasco <germsvel@gmail.com>
2020-01-27 19:42:29 -0500

    Update rake_spec
    Updates the rake_spec to expect the newer erb tags with spaces inside
    and a double quoted string inside the `t` method.

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -2,16 +2,6 @@ describe 'tasks' do
   before(:each) { FileUtils.cp_r("#{Rails.root}/spec/files", "#{Rails.root}/spec/tmp") }
   after(:each) { FileUtils.rm_rf("#{Rails.root}/spec/tmp") }
 
-  describe 'i18n:extract_html:list' do
-    it 'Returns a list of matched data' do
-      expect do
-        expect do
-          Rake::Task['i18n:extract_html:list'].invoke('spec/tmp/**/*.erb')
-        end.to output(%r{Found ".+" in [a-zA-Z\/\-\.]+}).to_stdout
-      end.not_to raise_exception
-    end
-  end
-
   describe 'i18n:extract_html:auto' do
     it 'Returns and replaces a list of matched data' do
       expect do
@@ -20,20 +10,8 @@ describe 'tasks' do
             Rake::Task['i18n:extract_html:auto'].invoke('spec/tmp/folder/minimal/*.erb')
           end.to output(/Found "Hello".*/).to_stdout
         end.not_to raise_exception
-      end.to change { File.read('spec/tmp/folder/minimal/file.html.erb') }.to('<%= link_to t(\'.hello\') %>')
-        .and(change { File.read('spec/tmp/folder/minimal/bug.html.erb') }.to('<div><%=t(\'.hello\')%></div>'))
-    end
-  end
-
-  describe 'i18n:extract_html:interactive' do
-    it 'Returns and replaces list of matched data' do
-      allow(STDIN).to receive(:gets).and_return('y')
-      expect do
-        expect do
-          Rake::Task['i18n:extract_html:interactive'].invoke('spec/tmp/folder/minimal/*.erb')
-        end.to output(/Found "Hello".*/).to_stdout
-      end.to change { File.read('spec/tmp/folder/minimal/file.html.erb') }.to('<%= link_to t(\'.y\') %>')
-        .and(change { File.read('spec/tmp/folder/minimal/bug.html.erb') }.to('<div><%=t(\'.y\')%></div>'))
+      end.to change { File.read('spec/tmp/folder/minimal/file.html.erb') }.to('<%= link_to t(".hello") %>')
+        .and(change { File.read('spec/tmp/folder/minimal/bug.html.erb') }.to('<div><%= t(".hello") %></div>'))
     end
   end
 end
