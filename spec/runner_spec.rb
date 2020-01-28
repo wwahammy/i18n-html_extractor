5931b531c410aa2b4a7d57bc940d981195622e07
German Velasco <germsvel@gmail.com>
2020-01-27 19:39:48 -0500

    Add runner_spec
    We add a runner_spec that though rough tests the core functionality of
    the `#run` method. It checks that the runner correctly adds the `t`
    methods in the `test.html.erb` file, and it checks that the correct
    translations are added to the english locale. We also remove the
    `ru.yml` file since translations for multiple languages was only part of
    the interactive mode, which we removed.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,22 +1,85 @@
 describe I18n::HTMLExtractor::Runner do
-  let(:folder) { 'spec/files/**/*.erb' }
-  subject { described_class.new(file_pattern: folder) }
-
   describe '#run' do
     it 'replaces text in files' do
-      skip 'TODO'
-    end
-  end
+      with_file_copy(source_file) do |copy|
+        runner = described_class.new(file_pattern: copy)
 
-  describe '#run_interactive' do
-    it 'replaces text in files' do
-      skip 'TODO'
-    end
-  end
+        runner.run
 
-  describe '#test_run' do
-    it 'lists all files that contains text to be extracted' do
-      expect { subject.test_run }.to output(%r{Found ".+" in [a-zA-Z\/\-\.]+}).to_stdout
+        expect_file_to_have_i18n(copy)
+        expect_translations_to_be_present
+      end
+    end
+
+    def expect_translations_to_be_present
+      raw_translations = YAML.load(File.read("config/locales/en.yml"))
+      translations = raw_translations.dig("en", "test_copied")
+
+      aggregate_failures "translations" do
+        expect(translations["hello"]).to eq "Hello"
+        expect(translations["hello_title"]).to eq "hello-title"
+        expect(translations["hi"]).to eq "Hi"
+        expect(translations["my_textarea_placeholder"]).to eq "my textarea placeholder"
+        expect(translations["my_input_placeholder"]).to eq "my input placeholder"
+      end
+    end
+
+    def expect_file_to_have_i18n(copy)
+      text = File.read(copy)
+      expected_text = File.read(translated_source_file)
+
+      expect(text).to eq expected_text
+    end
+
+    def with_file_copy(source_file)
+      copy = create_copy_of_file_to_edit(source_file)
+      copy_existing_translations
+
+      yield copy
+
+      clean_up_config_locale_file
+      remove_file(copy)
+    end
+
+    def copy_existing_translations
+      translations = YAML.load(File.read("config/locales/en.yml"))
+      translations["en"]["test_copied"] = translations["en"]["test"]
+      File.open("config/locales/en.yml", "w") do |file|
+        file.rewind
+        file.write(translations.to_yaml)
+        file.close
+      end
+    end
+
+    def clean_up_config_locale_file
+      translations = YAML.load(File.read("config/locales/en.yml"))
+      translations["en"].delete("test_copied")
+      File.open("config/locales/en.yml", "w") do |file|
+        file.rewind
+        file.write(translations.to_yaml)
+        file.close
+      end
+    end
+
+    def remove_file(copy)
+      FileUtils.rm(copy)
+    end
+
+    def create_copy_of_file_to_edit(source_file)
+      FileUtils.cp(source_file, copied_file)
+      copied_file
+    end
+
+    def copied_file
+      "spec/files/test_copied.html.erb"
+    end
+
+    def translated_source_file
+      "spec/files/test_translated.html.erb"
+    end
+
+    def source_file
+      "spec/files/test.html.erb"
     end
   end
 end
